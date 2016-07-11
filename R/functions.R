29c3b11f2560b35385ecb117309d474a78cce584
MTurgeon <maxime.turgeon@mail.mcgill.ca>
2016-07-11 17:35:41 -0600

    update R wrappers

--- R/functions.R
+++ R/functions.R
@@ -1,55 +1,24 @@
-#' Incomplete beta function
-#'
-#' Computes the incomplete beta function.
-#'
-#' This is defined as the integral from 0 to \code{x} of the function
-#' \code{t^(a-1)(1-t)^(b-1)}.
-#'
-#' @param x Value between 0 and 1 at which to evaluate the incomplete beta
-#'   function.
-#' @param a non-negative numeric
-#' @param b non-negative numeric
-#' @return Returns the value of the incomplete beta function at x.
-#' @export
-incompleteBeta <- function(x, a, b){
-    if(any(c(length(x), length(a), length(b)) != 1)) {
-        stop("None of the parameters can be vectors of length > 1", call. = FALSE)
-    }
-    stopifnot(x >= 0, x <= 1, a >= 0, b >= 0)
-
-    # Special cases
-    if(a == 0 || b == 0) {
-        val <- Inf
-    } else {
-        if(x == 0) val <- 0
-        if(x == 1) val <- beta(a, b)
-        if(x > 0 && x < 1) val <- incompleteBeta_C(x, alpha = a, beta = b)
-    }
-
-    return(val)
-}
-
 #' Distribution of the largest root
 #'
 #' Computes the cumulative distribution function of the largest root in the single and double Wishart setting.
 #'
 #' @param x Value at which to compute the CDF.
 #' @param s,m,n Parameters of the double Wishart setting. See Details.
+#' @param mprec Logical. Should we use gmp for high precision computations?
 #' @return Returns the value of the CDF at \code{x}.
 #' @export
 #' @aliases doubleWishart singleWishart
 #' @rdname largestRoot
-doubleWishart <- function(x, s, m, n) {
-    doubleWishart_C(x, s, m, n)
+doubleWishart <- function(x, s, m, n, mprec = TRUE) {
+    doubleWishart_raw(x, s, m, n, mprec)
 }
 
 #' @param n_min,n_max Parameters of the single Wishart setting. See details.
-#' @param mprec Logical. Should we use gmp for high precision computations?
 #' @export
 #' @rdname largestRoot
 singleWishart <- function(x, n_min, n_max, mprec = TRUE) {
     if (mprec) {
-        result <- singleWishart_mpfr(x, n_min, n_max)
+        result <- singleWishart_raw(x, n_min, n_max, mprec)
     } else {
         # There are some special cases
         specialCases <- list(c(2,2),
@@ -64,9 +33,13 @@ singleWishart <- function(x, n_min, n_max, mprec = TRUE) {
             if (all(c(n_min, n_max) == c(3,3))) result <- F33(x)
             if (all(c(n_min, n_max) == c(4,4))) result <- F44(x)
         } else {
-            result <- singleWishart_C(x, n_min, n_max)
+            result <- singleWishart_raw(x, n_min, n_max)
         }
     }
 
     return(result)
 }
+
+#' @useDynLib rootWishart
+#' @importFrom Rcpp sourceCpp
+NULL
--- src/RcppExports.cpp
+++ src/RcppExports.cpp
@@ -6,20 +6,6 @@
 
 using namespace Rcpp;
 
-// doubleWishart_C
-double doubleWishart_C(double x, int s, double m, double n);
-RcppExport SEXP rootWishart_doubleWishart_C(SEXP xSEXP, SEXP sSEXP, SEXP mSEXP, SEXP nSEXP) {
-BEGIN_RCPP
-    Rcpp::RObject __result;
-    Rcpp::RNGScope __rngScope;
-    Rcpp::traits::input_parameter< double >::type x(xSEXP);
-    Rcpp::traits::input_parameter< int >::type s(sSEXP);
-    Rcpp::traits::input_parameter< double >::type m(mSEXP);
-    Rcpp::traits::input_parameter< double >::type n(nSEXP);
-    __result = Rcpp::wrap(doubleWishart_C(x, s, m, n));
-    return __result;
-END_RCPP
-}
 // doubleWishart_raw
 double doubleWishart_raw(double x, int s, double m, double n, bool mp);
 RcppExport SEXP rootWishart_doubleWishart_raw(SEXP xSEXP, SEXP sSEXP, SEXP mSEXP, SEXP nSEXP, SEXP mpSEXP) {
@@ -35,19 +21,6 @@ BEGIN_RCPP
     return __result;
 END_RCPP
 }
-// singleWishart_mpfr
-double singleWishart_mpfr(double xx, int n_min, int n_max);
-RcppExport SEXP rootWishart_singleWishart_mpfr(SEXP xxSEXP, SEXP n_minSEXP, SEXP n_maxSEXP) {
-BEGIN_RCPP
-    Rcpp::RObject __result;
-    Rcpp::RNGScope __rngScope;
-    Rcpp::traits::input_parameter< double >::type xx(xxSEXP);
-    Rcpp::traits::input_parameter< int >::type n_min(n_minSEXP);
-    Rcpp::traits::input_parameter< int >::type n_max(n_maxSEXP);
-    __result = Rcpp::wrap(singleWishart_mpfr(xx, n_min, n_max));
-    return __result;
-END_RCPP
-}
 // singleWishart_raw
 double singleWishart_raw(double x, int n_min, int n_max, bool mp);
 RcppExport SEXP rootWishart_singleWishart_raw(SEXP xSEXP, SEXP n_minSEXP, SEXP n_maxSEXP, SEXP mpSEXP) {
@@ -59,32 +32,6 @@ BEGIN_RCPP
     Rcpp::traits::input_parameter< int >::type n_max(n_maxSEXP);
     Rcpp::traits::input_parameter< bool >::type mp(mpSEXP);
     __result = Rcpp::wrap(singleWishart_raw(x, n_min, n_max, mp));
-    return __result;
-END_RCPP
-}
-// incompleteBeta_C
-double incompleteBeta_C(double x, double alpha, double beta);
-RcppExport SEXP rootWishart_incompleteBeta_C(SEXP xSEXP, SEXP alphaSEXP, SEXP betaSEXP) {
-BEGIN_RCPP
-    Rcpp::RObject __result;
-    Rcpp::RNGScope __rngScope;
-    Rcpp::traits::input_parameter< double >::type x(xSEXP);
-    Rcpp::traits::input_parameter< double >::type alpha(alphaSEXP);
-    Rcpp::traits::input_parameter< double >::type beta(betaSEXP);
-    __result = Rcpp::wrap(incompleteBeta_C(x, alpha, beta));
-    return __result;
-END_RCPP
-}
-// mgamma_C
-double mgamma_C(double x, int m, bool logar);
-RcppExport SEXP rootWishart_mgamma_C(SEXP xSEXP, SEXP mSEXP, SEXP logarSEXP) {
-BEGIN_RCPP
-    Rcpp::RObject __result;
-    Rcpp::RNGScope __rngScope;
-    Rcpp::traits::input_parameter< double >::type x(xSEXP);
-    Rcpp::traits::input_parameter< int >::type m(mSEXP);
-    Rcpp::traits::input_parameter< bool >::type logar(logarSEXP);
-    __result = Rcpp::wrap(mgamma_C(x, m, logar));
     return __result;
 END_RCPP
 }
