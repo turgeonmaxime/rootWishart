525c021aefe4125458a630423869d9e1eff8a04d
Maxime Turgeon <turgeon.maxime@gmail.com>
2020-06-01 14:15:54 -0600

    update travis yaml file

--- tests/tinytest.R
+++
@@ -1,5 +0,0 @@
-if (requireNamespace("tinytest", quietly = TRUE) &&
-    utils::packageVersion("tinytest") >= "1.0.0") {
-    tinytest::test_package("rootWishart",
-                           ncpu = getOption("Ncpus", 1))
-}
